d57a03599a128250f0016c17fc684a0494a783ad
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2018-10-16 07:52:02 -0300

    Criado o controller de autenticação

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,4 +7,8 @@ module.exports = function(application){
 	application.get('/', function(req, res){
 		application.app.controllers.IndexController.index(application, req, res);		
 	});
+
+	application.post('/autenticar', function(req, res){
+		application.app.controllers.IndexController.autenticar(application, req, res);		
+	});
 }
