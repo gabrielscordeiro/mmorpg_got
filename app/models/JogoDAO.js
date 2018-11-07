f9b37b3cc670ec5dda2a33fa984f9a0987b9670a
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2018-11-07 12:48:49 -0200

    Atualizando moedas

--- app/models/JogoDAO.js
+++ app/models/JogoDAO.js
@@ -61,6 +61,27 @@ JogoDAO.prototype.acao = function (acao) {
 
             collection.insert(acao);
 
+           
+        });
+
+        mongoClient.collection('jogo', function (error, collection) {
+            var moedas = null;
+
+            switch (parseInt(acao.acao)) {
+                case 1: moedas = -2 * acao.quantidade; break;
+                case 2: moedas = -3 * acao.quantidade; break;
+                case 3: moedas = -1 * acao.quantidade; break;
+                case 4: moedas = -1 * acao.quantidade; break;
+            }
+
+            collection.update({
+                usuario: acao.usuario
+            }, {
+                $inc:{//inc faz um incremento com o valor passado por parametro com o valor dentro da chave em questão
+                    moeda: moedas
+                },
+            });
+
             mongoClient.close();
         });
     });
